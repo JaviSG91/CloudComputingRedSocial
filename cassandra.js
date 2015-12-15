a7ad958549a0c17f35bd97cebdbbaa999e7839d2
Javier Salido Gómez <javisago91@gmail.com>
2015-12-15 03:59:39 -0800

    new

--- cassandra.js
+++ cassandra.js
@@ -1,6 +1,131 @@
+var upsertSong = 'INSERT INTO simplex.usuarios (id, nombre, apellidos)  '
+    + 'VALUES(?, ?, ?, ?, ?, ?);';
+var getSongById = 'SELECT * FROM keysp.usuarios WHERE username = ?;';
+var contador = 'SELECT COUNT(*) FROM keysp.usuarios;';
+
+
 var cassandra = require('cassandra-driver');
 var assert = require('assert');
 var async = require('async');
+var express=require('express'); 
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var client = new cassandra.Client( {contactPoints: ['127.0.0.1'] } );
+client.set
+client.connect(function(err, result) {
+    console.log('Connected.');
+});
+
+app.set('ip', process.env.IP|| '0.0.0.0');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+    res.send('Bienvenido');
+     
+});
+
+
+app.post('/keyspace', function(req, res) {
+    var result="";
+    client.execute("CREATE KEYSPACE IF NOT EXISTS keysp WITH replication " + 
+                   "= {'class' : 'SimpleStrategy', 'replication_factor' : 3};",
+                   afterExecution('Error: ', 'Keyspace created.', res));
+    res.send("keyspace creado");
+});
+
+app.post('/tables', function(req, res) {
+    async.parallel([
+        function(next) {
+            client.execute('CREATE TABLE IF NOT EXISTS keysp.usuarios (' +
+                'username uuid PRIMARY KEY,' +
+                'nombre text,' +
+                'apellido text,' +
+                ');',
+                next);
+        }   
+    ], afterExecution('Error: ', 'Tables created.' , res));
+	res.send("tablas creadas");
+});
+
+app.post('/user', function(req, res) {
+    var id = null;
+    if ( ! req.body.hasOwnProperty('username')) {
+        id = cassandra.types.uuid();
+    } else {
+        id = req.body.username;
+    }
+    client.execute(upsertSong,
+        [id, req.body.nombre, req.body.apellido],
+        afterExecution('Error: ', 'Usuario ' + req.body.nombre + ' upserted.', res));
+    res.send(id + " is stored");
+});
+
+/*app.get('/userb', function(req, res) {
+    var d;
+    creaKey(d);
+    client.execute("SELECT COUNT(*) FROM keysp.usuarios", function(err, result) {
+        if (err) {
+	    res.status(404).send({ msg : err });
+           // res.status(404).send({ msg : 'User not found.' });
+        } else {
+            res.json(result);        }
+    });
+
+});*/
+
+function afterExecution(errorMsessage, successMessage) {
+    return function(err, result) {
+        if (err) {
+            return "error";
+        } else {
+            return "bien";
+        }
+    }
+}
+
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+module.exports = app;
+
+
+/*var cassandra = require('cassandra-driver');
+var assert = require('assert');
+var async = require('async');
+var express=require('express'); 
+var app = express(); 
+
+app.set('ip', process.env.IP|| '0.0.0.0');
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function (req, res) { 
+	var client = new cassandra.Client({contactPoints: ['127.0.0.1','127.0.0.2'], keyspace: 'mykeyspace'});
+	
+	 client.execute("SELECT nombre FROM Usuarios where username='javisg'", function (err, result) {
+           if (!err){
+               if ( result.rows.length > 0 ) {
+                   var user = result.rows[0];
+                   res.send(user.nombre);
+               } else {
+                   res.send("nada");
+               }
+           }
+	   else
+		res.send("error");
+ 
+       });
+	
+	
+	//res.send('Bienvenido');
+
+}); 
+ 
+
 
 
  //Connect to the cluster
@@ -22,4 +147,7 @@ Mensajeria.DevuelveNombre = function(req,res){
 	});
 }
 
-module.exports = Mensajeria;
+app.listen(app.get('port'), function() {
+  console.log('Puerto:', app.get('port'));
+});
+module.exports = app;*/
--- test/test.js
+++ test/test.js
@@ -1,9 +1,42 @@
-var mensajes = require("../cassandra");
+var app = require("../cassandra");
+var request = require("supertest"); 
 var assert = require("assert");
 
 describe('Mensajeria', function(){
 
-	it('Nombre', function(done){
+
+	it("Index", function(done) { 
+		request(app) 
+			.get("/") 
+			.expect("Content-Type", /text\/html/) 
+			.expect(200, done); 
+	}); 
+
+	it("key", function(done) { 
+ 		request(app) 
+ 			.post("/keyspace") 
+			.expect("Content-Type", /text\/html/) 
+			.expect(200, done); 
+	}); 
+	it("tablas", function(done) { 
+ 		request(app) 
+ 			.post("/tables") 
+			.expect("Content-Type", /text\/html/) 
+			.expect(200, done); 
+	}); 
+	it("Prueba", function(done) { 
+		var user = { username : 'javisg', nombre:"Javier", apellido : 'Salido'};
+ 		request(app) 
+			.post("/user") 
+			.send(user)
+			.expect("Content-Type", /text\/html/) 
+			.expect("javisg is stored", done); 
+	}); 
+
+	
+
+
+	/*it('Nombre', function(done){
 			mensajes.DevuelveNombre(
 				"javisg",function(err,data){
 				assert.equal(data.nombre,"Javier");
@@ -12,7 +45,7 @@ describe('Mensajeria', function(){
 
 			});
 		 	
-	});
+	});*/
 
 
 
